73d88340e3a9ce9aa6184487848a75781a6b2967
5682 <sohail.shaik@wisseninfotech.com>
2024-02-18 15:51:16 +0530

    added few css properties for more responsiveness

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -210,7 +210,7 @@ function VideoPlayer({ videoSrc, isVideoSelected }) {
             <option value={0.25}>0.25</option>
             <option value={0.50}>0.50</option>
             <option value={0.75}>0.75</option>
-            <option value={1}>Normal</option>
+            <option value={1}>1X</option>
             <option value={1.25}>1.25</option>
             <option value={1.5}>1.5</option>
             <option value={1.75}>1.75</option>
